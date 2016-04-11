3b05bf2a20c5db518e1a6662f4eb4ad480a3b9d3
Kurt Friedrich <kurt.is.friedrich@gmail.com>
2016-04-11 18:08:26 -0400

    created users page to show and follows users. follow functionality broken on page

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -5,7 +5,7 @@ class Ship < ActiveRecord::Base
   def self.timeline(user)
     following_ids = user.followees(User, pluck: :id)
     all_ids= following_ids << user.id
-    Ship.where(user_id: all_ids).order("created_at DESC")
+    self.where(user_id: all_ids).order("created_at DESC")
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   resources :users, except: [:index] do
     resources :ships, except: [:edit, :update]
+    resources :users, only: [:index]
   end
 
   resources :users, only: [:show] do
