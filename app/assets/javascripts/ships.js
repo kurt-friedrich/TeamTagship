5027d4680b75a89ef15ef8d382d8a624d58d3262
Kurt Friedrich <kurt.is.friedrich@gmail.com>
2016-04-12 15:42:27 -0400

    added character counter for new ships

--- app/assets/javascripts/ships.js
+++ app/assets/javascripts/ships.js
@@ -1,3 +1,6 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+$(document).ready( function() {
+  $("#ship_body").on("keyup", function(){
+    var length = $(this).val().length;
+    $("#body-count").html(length);
+  });
+});
